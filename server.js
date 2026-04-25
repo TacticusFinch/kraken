f7c5c9ab3db8f50a2645f5ff9e8ca2065e8e1298
TacticusFinch <nagajolokias@gmail.com>
2026-04-26 04:09:35 +0800

    Настроил CORS заголовки в server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(express.json());
 
 // Заголовки для SharedArrayBuffer
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
     res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     next();
